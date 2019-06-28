704157b9240d671ba0434bf72dece82b076e77f8
Marc Zyngier <maz@kernel.org>
2019-06-28 15:23:43 +0100

    BACKPORT: UPSTREAM: KVM: arm64: timers: Move timer registers to the sys_regs file
    Move the timer gsisters to the sysreg file. This will further help when
    they are directly changed by a nesting hypervisor in the VNCR page.
    
    This requires moving the initialisation of the timer struct so that some
    of the helpers (such as arch_timer_ctx_index) can work correctly at an
    early stage.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    
    (cherry picked from commit 41ce82f63c0624556b94b90551e4f70a0c913879)
    
    Conflicts:
    	arch/arm64/include/asm/kvm_host.h (context)
    
    BUG=b:295256641
    TEST=Observed difference in CLOCK_BOOTTIME and CLOCK_MONOTONIC in guest
        after suspend.
    
    Change-Id: Ie09a1bbddbaed0a18d66d24001c418217e934620
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5541534
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -162,6 +162,12 @@ enum vcpu_sysreg {
 	APGAKEYLO_EL1,
 	APGAKEYHI_EL1,
 
+	CNTVOFF_EL2,
+	CNTV_CVAL_EL0,
+	CNTV_CTL_EL0,
+	CNTP_CVAL_EL0,
+	CNTP_CTL_EL0,
+
 	/* 32bit specific registers. Keep them at the end of the range */
 	DACR32_EL2,	/* Domain Access Control Register */
 	IFSR32_EL2,	/* Instruction Fault Status Register */
