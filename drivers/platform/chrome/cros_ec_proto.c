8e219820b138237383266aa82db9f6ad79879785
Guenter Roeck <linux@roeck-us.net>
2020-08-22 08:08:55 -0700

    UPSTREAM: platform/input: cros_ec: Replace -ENOTSUPP with -ENOPROTOOPT
    -ENOTSUPP is not a SUSV4 error code and should not be used. Use
    -ENOPROTOOPT instead to report EC_RES_INVALID_VERSION responses
    from the EC. This matches match the NFS response for unsupported
    protocol versions.
    
    Cc: Gwendal Grignou <gwendal@chromium.org>
    Cc: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Cc: Prashant Malani <pmalani@chromium.org>
    Cc: Brian Norris <briannorris@chromium.org>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    (cherry picked from commit b4e452b5e97dc70d529475168a37ad7394994b8b)
    
    BUG=b:355000322
    TEST=Install kernel on Zork and make sure affected FPMCU firmware works
         properly
    
    Change-Id: I7496bc4993091f50e02065c16cebcc23a49a3306
    Signed-off-by: Patryk Duda <patrykd@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5737914
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- drivers/platform/chrome/cros_ec_proto.c
+++ drivers/platform/chrome/cros_ec_proto.c
@@ -589,7 +589,7 @@ EXPORT_SYMBOL(cros_ec_cmd_xfer);
  *
  * Return:
  * >=0 - The number of bytes transferred
- * -ENOTSUPP - Operation not supported
+ * -ENOPROTOOPT - Operation not supported
  * -EPROTO - Protocol error
  */
 int cros_ec_cmd_xfer_status(struct cros_ec_device *ec_dev,
@@ -603,7 +603,7 @@ int cros_ec_cmd_xfer_status(struct cros_ec_device *ec_dev,
 	} else if (msg->result == EC_RES_INVALID_VERSION) {
 		dev_dbg(ec_dev->dev, "Command invalid version (err:%d)\n",
 			msg->result);
-		return -ENOTSUPP;
+		return -ENOPROTOOPT;
 	} else if (msg->result != EC_RES_SUCCESS) {
 		dev_dbg(ec_dev->dev, "Command result (err: %d)\n", msg->result);
 		return -EPROTO;
