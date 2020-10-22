986aea419520d2dc9e0f17ded4a7394825de670a
Mathy Vanhoef <Mathy.Vanhoef@kuleuven.be>
2020-10-22 15:36:14 +0400

    CHROMIUM: iwl7000: mac80211: properly handle A-MSDUs that start with an RFC 1042 header
    Properly parse A-MSDUs whose first 6 bytes happen to equal a rfc1042
    header. This can occur in practice when the destination MAC address
    equals AA:AA:03:00:00:00. More importantly, this simplifies the next
    patch to mitigate A-MSDU injection attacks.
    
    BUG=b:187853103, b:187853926
    TEST=Verified wifi_matfunc/perf
    
    Signed-off-by: Mathy Vanhoef <Mathy.Vanhoef@kuleuven.be>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: d69b733a5e85e7600c38aff8773378161c9d3d39
    Signed-off-by: Abhishek Kumar <kuabhs@chromium.org>
    Change-Id: I23dd5d685fe16d3b0ec8106e8f01b59f499dffed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2888688
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- drivers/net/wireless/iwl7000/hdrs/mac80211-bp.h
+++ drivers/net/wireless/iwl7000/hdrs/mac80211-bp.h
@@ -1642,8 +1642,6 @@ const u8 *bp_cfg80211_find_ie_match(u8 eid, const u8 *ies, int len,
 
 #define NL80211_EXT_FEATURE_MU_MIMO_AIR_SNIFFER -1
 
-int ieee80211_data_to_8023_exthdr(struct sk_buff *skb, struct ethhdr *ehdr,
-				  const u8 *addr, enum nl80211_iftype iftype);
 /* manually renamed to avoid symbol issues with cfg80211 */
 #define ieee80211_amsdu_to_8023s iwl7000_ieee80211_amsdu_to_8023s
 void ieee80211_amsdu_to_8023s(struct sk_buff *skb, struct sk_buff_head *list,
@@ -2343,10 +2341,6 @@ static inline bool iwl7000_cfg80211_rx_control_port(struct net_device *dev,
 #define WIPHY_PARAM_TXQ_MEMORY_LIMIT	0
 #define WIPHY_PARAM_TXQ_QUANTUM		0
 
-#define ieee80211_data_to_8023_exthdr iwl7000_ieee80211_data_to_8023_exthdr
-int ieee80211_data_to_8023_exthdr(struct sk_buff *skb, struct ethhdr *ehdr,
-				  const u8 *addr, enum nl80211_iftype iftype,
-				  u8 data_offset);
 #else
 static inline int
 backport_cfg80211_sinfo_alloc_tid_stats(struct station_info *sinfo, gfp_t gfp)
@@ -2707,3 +2701,17 @@ static inline void cfg80211_bss_iter(struct wiphy *wiphy,
 	 */
 }
 #endif /* CFG80211_VERSION < KERNEL_VERSION(5,3,0) */
+
+#if CFG80211_VERSION < KERNEL_VERSION(5,13,0)
+#define ieee80211_data_to_8023_exthdr iwl7000_ieee80211_data_to_8023_exthdr
+int ieee80211_data_to_8023_exthdr(struct sk_buff *skb, struct ethhdr *ehdr,
+				  const u8 *addr, enum nl80211_iftype iftype,
+				  u8 data_offset, bool is_amsdu);
+
+#define ieee80211_data_to_8023 iwl7000_ieee80211_data_to_8023
+static inline int ieee80211_data_to_8023(struct sk_buff *skb, const u8 *addr,
+					 enum nl80211_iftype iftype)
+{
+	return ieee80211_data_to_8023_exthdr(skb, NULL, addr, iftype, 0, false);
+}
+#endif /* CFG80211_VERSION < KERNEL_VERSION(5,13,0) */
