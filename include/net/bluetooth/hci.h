0c2615bfa5d75fe38d73d4fe293c4e4e662051fb
Kiran K <kiran.k@intel.com>
2021-01-04 20:26:04 +0530

    CHROMIUM: Bluetooth: Fix for LE mouse lag issue on StP
    Mouse (HOGP) lag is observed on Intel StP controller in KeyChron + HOGP
    mouse use case due to conflict in achor points. A dirty fix added to
    avoid the collision by restricting connection interval min/max to 7.25ms
    for LE connections when KeyChron BR/EDR device connected.
    
    Note: this is a reland of CL:2635321 with a fix of productId.
    
    BUG=b:154957234,b:192820264
    TEST=Sanity test with HOGP mouse + KeyChron BR/EDR keyboard combination
    
    Signed-off-by: Kiran K <kiran.k@intel.com>
    Signed-off-by: Chethan T N <chethan.tumkur.narayan@intel.com>
    Signed-off-by: Sonny Sasaka <sonnysasaka@chromium.org>
    
    Change-Id: I90e1ff6851100c205cc660573638ea766def2793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3169995
    Reviewed-by: Alain Michaud <alainm@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Tested-by: Sonny Sasaka <sonnysasaka@chromium.org>

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -192,6 +192,9 @@ enum {
 	 * This quirk must be set before hci_register_dev is called.
 	 */
 	HCI_QUIRK_WIDEBAND_SPEECH_SUPPORTED,
+
+	/* This quirk will be set when running on Intel SdP/StP controller */
+	HCI_QUIRK_RESTRICT_CONN_PARAMS,
 };
 
 /* HCI device flags */
