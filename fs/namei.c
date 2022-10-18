9553c94da594806279a65198c7d6102cf2130f0c
Günther Noack <gnoack3000@gmail.com>
2022-10-18 20:22:06 +0200

    UPSTREAM: BACKPORT: security: Create file_truncate hook from path_truncate hook
    Like path_truncate, the file_truncate hook also restricts file
    truncation, but is called in the cases where truncation is attempted
    on an already-opened file.
    
    This is required in a subsequent commit to handle ftruncate()
    operations differently to truncate() operations.
    
    Acked-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Acked-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Paul Moore <paul@paul-moore.com>
    Change-Id: Ic0177884067f180f341eb3f224362eaeb2578b05
    Signed-off-by: Günther Noack <gnoack3000@gmail.com>
    Link: https://lore.kernel.org/r/20221018182216.301684-2-gnoack3000@gmail.com
    Signed-off-by: Mickaël Salaün <mic@digikod.net>
    (cherry picked from commit 3350607dc5637be2563f484dcfe2fed456f3d4ff)
    
    Backport because replacing security_path_truncate() with
    security_file_truncate() is seen as diff by review-o-matic, even though
    comparing with the original CL at
    https://lore.kernel.org/all/20221018182216.301684-2-gnoack3000@gmail.com/#Z31fs:namei.c
    does not show any semantic diff between the two CL’s.
    
    BUG=b:301855023
    TEST=Verified Landlock is available on betty
    
    Change-Id: I880fefcac8532ebafc20335bc19aca422cf27638
    Signed-off-by: Ben Scarlato <akhna@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4956973
    Reviewed-by: Jeff Xu <jeffxu@google.com>
    Reviewed-by: Allen Webb <allenwebb@google.com>

--- fs/namei.c
+++ fs/namei.c
@@ -3061,7 +3061,7 @@ static int handle_truncate(struct file *filp)
 	 */
 	error = locks_verify_locked(filp);
 	if (!error)
-		error = security_path_truncate(path);
+		error = security_file_truncate(filp);
 	if (!error) {
 		error = do_truncate(path->dentry, 0,
 				    ATTR_MTIME|ATTR_CTIME|ATTR_OPEN,
