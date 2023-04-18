1b415cbdc4d2258cb37610c4a66039ebffdec9f6
Douglas Anderson <dianders@chromium.org>
2023-04-17 17:15:31 -0700

    FIXUP: CHROMIUM: hardlockup: detect hard lockups without NMIs using secondary cpus (sysctl)
    The CHROMIUM patch accidentally didn't expose the hardlockup panic
    sysctls based on the right config. Fix it.
    
    NOTE: Only one of these two sysctls actually does something with the
    current buddy detector. You can turn on/off the hard lockup detector
    but it doesn't (yet) support tracing other CPUs.
    
    UPSTREAM-TASK=b:172213097
    BUG=b:278598383, b:278594093, b:197061987, b:172213097
    TEST=echo HARDLOCKUP > /sys/kernel/debug/provoke-crash/DIRECT
    
    Change-Id: Id88d1fb603308e7210c30e42bb6e4e6a4be65a0c
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4438219
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    (cherry picked from commit 71986679fe52d94286f9051f09b958ecf582c7fc)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5008220
    Commit-Queue: Guenter Roeck <groeck@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1034,7 +1034,7 @@ static struct ctl_table kern_table[] = {
 	},
 #endif /* CONFIG_SMP */
 #endif
-#ifdef CONFIG_HARDLOCKUP_DETECTOR
+#ifdef CONFIG_HARDLOCKUP_DETECTOR_CORE
 	{
 		.procname	= "hardlockup_panic",
 		.data		= &hardlockup_panic,
