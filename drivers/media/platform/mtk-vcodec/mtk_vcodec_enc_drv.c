9b72f9714a9b115bf8854ffebd46b8c5927c69f8
Yunfei Dong <yunfei.dong@mediatek.com>
2023-05-30 20:29:07 +0800

    BACKPORT: media: mediatek: vcodec: Add encode to support dbgfs
    Add encode to support dbgfs.
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    (cherry picked from commit cc770218226534d921d2adb0819e00014ce471c5)
    
    The driver is at drivers/media/platform/mediatek/vcodec/ in the upstream,
    while it is at drivers/media/platform/mtk-vcodec/ here. (This can be
    done by git-cherry-pick automatically.)
    
    BUG=b:317227622
    TEST=emerge-asurada chromeos-kernel-5_4
    
    Change-Id: Ib9b5555a642123cbdc7c87748565675b77823876
    Signed-off-by: Yu-Che Cheng <giver@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5185820
    Reviewed-by: Fei Shao <fshao@chromium.org>

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_enc_drv.c
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_enc_drv.c
@@ -374,6 +374,7 @@ static int mtk_vcodec_probe(struct platform_device *pdev)
 		goto err_enc_reg;
 	}
 
+	mtk_vcodec_dbgfs_init(dev, true);
 	mtk_v4l2_debug(0, "encoder %d registered as /dev/video%d",
 		       dev->venc_pdata->core_id, vfd_enc->num);
 
@@ -466,6 +467,7 @@ static int mtk_vcodec_enc_remove(struct platform_device *pdev)
 	if (dev->vfd_enc)
 		video_unregister_device(dev->vfd_enc);
 
+	mtk_vcodec_dbgfs_deinit(dev);
 	v4l2_device_unregister(&dev->v4l2_dev);
 	mtk_vcodec_release_enc_pm(dev);
 	mtk_vcodec_fw_release(dev->fw_handler);
