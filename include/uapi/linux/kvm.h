f79defe3c750e95ce7cf290a0c01d349058ab17e
David Dai <davidai@google.com>
2023-06-14 10:44:50 +0900

    CHROMIUM: BACKPORT: FROMLIST: kvm: arm64: Add support for get_freqtbl service
    This service allows guests to query the host for the frequency table
    of the CPU that the vCPU is currently running on.
    
    Co-developed-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: David Dai <davidai@google.com>
    (am from https://patchwork.kernel.org/patch/13194985/)
    (also found at https://lore.kernel.org/r/20230330224348.1006691-5-davidai@google.com)
    
    Conflicts:
       arch/arm64/include/uapi/asm/kvm.h
       arch/arm64/kvm/hypercalls.c
       arch/arm64/kvm/arm.c
       include/linux/arm-smccc.h
       include/uapi/linux/kvm.h
       tools/arch/arm64/include/uapi/asm/kvm.h
    
    BUG=b:249685960
    TEST=Build, deploy and run autologin.py to confirm arcvm boot.
    UPSTREAM-TASK=b:256983979
    
    Change-Id: I506002d596a2939192e8e94061531837936f9656
    Signed-off-by: Masami Hiramatsu <mhiramat@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4406120
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1056,6 +1056,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_PTP_KVM 195
 #define KVM_CAP_GET_CUR_CPUFREQ 512
 #define KVM_CAP_UTIL_HINT 513
+#define KVM_CAP_GET_CPUFREQ_TBL 514
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
