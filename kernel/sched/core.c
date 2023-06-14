e5bd4b2be5dc132da85f230bdfa93bd00698f5d3
David Dai <davidai@google.com>
2023-06-14 10:44:49 +0900

    CHROMIUM: BACKPORT: FROMLIST: sched/fair: Add util_guest for tasks
    For virtualization usecases, util_est and util_avg currently tracked
    on the host aren't sufficient to accurately represent the workload on
    vCPU threads, which results in poor frequency selection and performance.
    For example, when a large workload migrates from a busy vCPU thread to
    an idle vCPU thread, it incurs additional DVFS ramp-up latencies
    as util accumulates.
    
    Introduce a new "util_guest" member as an additional PELT signal that's
    independently updated by the guest. When used, it's max aggregated to
    provide a boost to both task_util and task_util_est.
    
    Updating task_util and task_util_est will ensure:
    -Better task placement decisions for vCPU threads on the host
    -Correctly updating util_est.ewma during dequeue
    -Additive util with other threads on the same runqueue for more
    accurate frequency responses
    
    Co-developed-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: David Dai <davidai@google.com>
    (am from https://patchwork.kernel.org/patch/13195195/)
    (also found at https://lore.kernel.org/r/20230331014356.1033759-2-davidai@google.com)
    
    Conflicts:
       kernel/sched/core.c
       kernel/sched/fair.c
    
    BUG=b:249685960
    TEST=Build
    UPSTREAM-TASK=b:256983979
    
    Change-Id: Ibf0e1bc6fc437807399bedde1ce0d108d1fa78da
    Signed-off-by: Masami Hiramatsu <mhiramat@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4406117
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1932,6 +1932,21 @@ static inline void uclamp_post_fork(struct task_struct *p) { }
 static inline void init_uclamp(void) { }
 #endif /* CONFIG_UCLAMP_TASK */
 
+#ifdef CONFIG_SMP
+static void __setscheduler_task_util(struct task_struct *p,
+				  const struct sched_attr *attr)
+{
+
+	if (likely(!(attr->sched_flags & SCHED_FLAG_UTIL_GUEST)))
+		return;
+
+	p->se.avg.util_guest = attr->sched_util_min;
+}
+#else
+static inline void __setscheduler_task_util(struct task_struct *p,
+					    const struct sched_attr *attr) { }
+#endif
+
 static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 {
 	if (!(flags & ENQUEUE_NOCLOCK))
@@ -6210,7 +6225,7 @@ recheck:
 			return -EINVAL;
 	}
 
-	if (attr->sched_flags & ~(SCHED_FLAG_ALL | SCHED_FLAG_SUGOV))
+	if (attr->sched_flags & ~(SCHED_FLAG_ALL | SCHED_FLAG_SUGOV | SCHED_FLAG_UTIL_GUEST))
 		return -EINVAL;
 
 	/*
@@ -6280,13 +6295,16 @@ recheck:
 		if (attr->sched_flags & SCHED_FLAG_SUGOV)
 			return -EINVAL;
 
+		if (attr->sched_flags & SCHED_FLAG_UTIL_GUEST)
+			return -EINVAL;
+
 		retval = security_task_setscheduler(p);
 		if (retval)
 			return retval;
 	}
 
 	/* Update task specific "requested" clamps */
-	if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP) {
+	if (attr->sched_flags & (SCHED_FLAG_UTIL_CLAMP | SCHED_FLAG_UTIL_GUEST)) {
 		retval = uclamp_validate(p, attr);
 		if (retval)
 			return retval;
@@ -6325,6 +6343,8 @@ recheck:
 		if (dl_policy(policy) && dl_param_changed(p, attr))
 			goto change;
 		if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP)
+			goto change;
+		if (attr->sched_flags & SCHED_FLAG_UTIL_GUEST)
 			goto change;
 
 		p->sched_reset_on_fork = reset_on_fork;
@@ -6415,6 +6435,7 @@ change:
 		__setscheduler_prio(p, newprio);
 	}
 	__setscheduler_uclamp(p, attr);
+	__setscheduler_task_util(p, attr);
 
 	if (queued) {
 		/*
