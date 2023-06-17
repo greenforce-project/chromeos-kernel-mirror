19a9c46f45e8d417f6e79808151dfc722b259c3e
Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
2023-06-17 01:13:03 +0000

    UPSTREAM: Bluetooth: hci_qca: Stop IBS timer during BT OFF
    The IBS timers are not stopped properly once BT OFF is triggered.
    we could see IBS commands being sent along with version command,
    so stopped IBS timers while Bluetooth is off.
    
    Fixes: 3e4be65eb82c ("Bluetooth: hci_qca: Add poweroff support during hci down for wcn3990")
    Signed-off-by: Panicker Harish <quic_pharish@quicinc.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit df1e5c51492fd93ffc293acdcc6f00698d19fedc)
    
    BUG=b:74019558
    TEST=tested on cheza.
    
    Signed-off-by: Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
    Change-Id: Iec167b0f7d54c9d42ad0ef252cb6d4119b90f799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4623204
    Reviewed-by: Sean Paul <sean@poorly.run>
    Tested-by: Guenter Roeck <groeck@chromium.org>
    Commit-Queue: Guenter Roeck <groeck@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- drivers/bluetooth/hci_qca.c
+++ drivers/bluetooth/hci_qca.c
@@ -1845,6 +1845,9 @@ static int qca_power_off(struct hci_dev *hdev)
 	hu->hdev->hw_error = NULL;
 	hu->hdev->cmd_timeout = NULL;
 
+	del_timer_sync(&qca->wake_retrans_timer);
+	del_timer_sync(&qca->tx_idle_timer);
+
 	/* Stop sending shutdown command if soc crashes. */
 	if (qca_is_wcn399x(soc_type)
 		&& qca->memdump_state == QCA_MEMDUMP_IDLE) {
