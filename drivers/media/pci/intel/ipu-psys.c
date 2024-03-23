63fe11757d3dfa8783cfdd05525db023d56ed62c
Sergey Senozhatsky <senozhatsky@chromium.org>
2024-03-23 04:02:58 +0000

    CHROMIUM: media: ipu6: fix ipu_psys_kbuf_unmap()
    When ipu_psys_kbuf_unmap() is called from the error path
    in ipu_psys_mapbuf_locked() kbuf->db_attach or kbuf->sgt
    can be IS_ERR(), so we need to check that.
    
    UPSTREAM-TASK=b:149068439
    UPSTREAM-TASK=b:149068672
    BUG=b:195092706
    TEST=CCA on voema
    
    Change-Id: If06fc2ae74471ab457459fc9f4a71ecf31510ab1
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5371677
    Reviewed-by: Bingbu Cao <bingbu.cao@intel.com>
    Reviewed-by: Ricardo Ribalda <ribalda@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5376399
    Reviewed-by: Hidenori Kobayashi <hidenorik@chromium.org>

--- drivers/media/pci/intel/ipu-psys.c
+++ drivers/media/pci/intel/ipu-psys.c
@@ -448,11 +448,11 @@ static inline void ipu_psys_kbuf_unmap(struct ipu_psys_kbuffer *kbuf)
 	kbuf->valid = false;
 	if (kbuf->kaddr)
 		dma_buf_vunmap(kbuf->dbuf, kbuf->kaddr);
-	if (kbuf->sgt)
+	if (!IS_ERR_OR_NULL(kbuf->sgt))
 		dma_buf_unmap_attachment(kbuf->db_attach,
 					 kbuf->sgt,
 					 DMA_BIDIRECTIONAL);
-	if (kbuf->db_attach)
+	if (!IS_ERR_OR_NULL(kbuf->db_attach))
 		dma_buf_detach(kbuf->dbuf, kbuf->db_attach);
 	dma_buf_put(kbuf->dbuf);
 
