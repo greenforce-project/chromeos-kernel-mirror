3badd2a20624df576d1256bb4df0effc9a5d022d
Johannes Berg <johannes.berg@intel.com>
2024-09-04 11:34:06 +0200

    CHROMIUM: iwl7000: mac80211: allow rate_control_rate_init() for links
    Andrei previously fixed an issue in the client where the NSS
    for links other than the primary/assoc/deflink isn't set. The
    same issue appears to exist on the AP side, because there's
    only a call to rate_control_rate_init() for the deflink, and
    not any other links.
    
    Rework the code a bit to do rate_control_rate_init() for links,
    even if it really doesn't work with software rate control yet,
    it does other things as well.
    
    Also add rate_control_rate_init_all_links() to actually do it
    properly when moving to ASSOC state in cfg80211.
    
    Change the explicit call to ieee80211_sta_init_nss() to instead
    be rate_control_rate_init() now in the client code, but also
    add a call to rate_control_rate_init() when a link is added in
    AP mode and the STA is already associated.
    
    This should fix the NSS initialization issue, and perhaps pave
    the way for actual software rate scaling a bit, in case anyone
    cares in the future, but that of course needs a lot more than
    just the init call.
    
    We still need to fix the rate control _update_ as well, and the
    sta_rc_update() driver method especially, but that will be in a
    different patch.
    
    Change-Id: I0376da02e9f5a30eaa1b5d0d01371ff09506d453
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 670b13535473e882bf9644fb072439cc8201ba33

--- drivers/net/wireless/iwl7000/mac80211/cfg.c
+++ drivers/net/wireless/iwl7000/mac80211/cfg.c
@@ -1557,7 +1557,7 @@ static int sta_apply_auth_flags(struct ieee80211_local *local,
 		 * before drv_sta_state() is called.
 		 */
 		if (!test_sta_flag(sta, WLAN_STA_RATE_CONTROL))
-			rate_control_rate_init(sta);
+			rate_control_rate_init_all_links(sta);
 
 		ret = sta_info_move_state(sta, IEEE80211_STA_ASSOC);
 		if (ret)
@@ -1954,7 +1954,7 @@ static int ieee80211_add_station(struct wiphy *wiphy, struct net_device *dev,
 	 */
 	if (!test_sta_flag(sta, WLAN_STA_TDLS_PEER) &&
 	    test_sta_flag(sta, WLAN_STA_ASSOC))
-		rate_control_rate_init(sta);
+		rate_control_rate_init_all_links(sta);
 
 	return sta_info_insert(sta);
 }
