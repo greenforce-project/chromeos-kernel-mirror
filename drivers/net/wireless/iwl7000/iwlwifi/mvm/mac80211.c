ad4fc233d724119f68f7b2233ae6a33be580ce1b
Johannes Berg <johannes.berg@intel.com>
2024-09-24 12:15:32 +0200

    CHROMIUM: iwl7000: mvm: restart device through NMI
    When some channel context manipulations fail, the device
    is going to be restarted to try to recover. Make this go
    through a real FW restart via an NMI so the transport is
    aware of it and can later handle escalation, and to make
    it easier to restructure the code later.
    
    Change-Id: I2e729f402db58a76cea620b6f62a02da49a10b48
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: 10af1ed41399046e74e4217bfd4e6ae7518729d6

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
@@ -5385,7 +5385,7 @@ out_reassign:
 
 out_restart:
 	/* things keep failing, better restart the hw */
-	iwl_mvm_nic_restart(mvm);
+	iwl_force_nmi(mvm->trans);
 	return ret;
 }
 
@@ -5421,7 +5421,7 @@ out_reassign:
 
 out_restart:
 	/* things keep failing, better restart the hw */
-	iwl_mvm_nic_restart(mvm);
+	iwl_force_nmi(mvm->trans);
 	return ret;
 }
 
