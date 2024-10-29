57cd376a03a341ccd4c2faf99e8cc3a9c2e4951c
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2024-10-29 13:55:49 +0200

    CHROMIUM: iwl7000: Revert "wifi: iwlwifi: mvm: clean up FW restart a bit"
    This reverts commit d7fe2b068072caa4156961b4dc70b3a9b0ff131e.
    This needs to be rewritten after 3aa5c2a02f74 is reverted
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: If98352fcacc2895df8164e85e37285c48c68a29b
    iwl7000-tree: c591e3e9f639facfca3f8a1a544601ae7739e9f6

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
@@ -5354,7 +5354,7 @@ out_reassign:
 
 out_restart:
 	/* things keep failing, better restart the hw */
-	iwl_mvm_nic_restart(mvm);
+	iwl_mvm_nic_restart(mvm, false);
 	return ret;
 }
 
@@ -5390,7 +5390,7 @@ out_reassign:
 
 out_restart:
 	/* things keep failing, better restart the hw */
-	iwl_mvm_nic_restart(mvm);
+	iwl_mvm_nic_restart(mvm, false);
 	return ret;
 }
 
