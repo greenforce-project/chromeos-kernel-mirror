d5cc8b448087249ce6b1ce58c37a6d5a44c2650b
Daniel Gabay <daniel.gabay@intel.com>
2024-11-19 00:18:30 +0200

    CHROMIUM: iwl7000: mld: free nvm_data in op mode start failure path
    mld->nvm_data is allocated when we load the firmware, so it should
    be freed if we fail later in the op mode start.
    
    Signed-off-by: Daniel Gabay <daniel.gabay@intel.com>
    Change-Id: I1f1a0a0a0b62cf6944686d5a768bda5aa8d979ed
    iwl7000-tree: bd6eb485f97e140eb3cf04e7ca4d04c8045175e3

--- drivers/net/wireless/iwl7000/iwlwifi/mld/mld.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mld/mld.c
@@ -349,7 +349,7 @@ iwl_op_mode_mld_start(struct iwl_trans *trans, const struct iwl_cfg *cfg,
 
 	ret = iwl_mld_leds_init(mld);
 	if (ret)
-		goto free_hw;
+		goto free_nvm;
 
 	ret = iwl_mld_alloc_scan_cmd(mld);
 	if (ret)
@@ -370,6 +370,8 @@ free_scan_cmd:
 	kfree(mld->scan.cmd);
 leds_exit:
 	iwl_mld_leds_exit(mld);
+free_nvm:
+	kfree(mld->nvm_data);
 free_hw:
 	ieee80211_free_hw(mld->hw);
 	return ERR_PTR(ret);
