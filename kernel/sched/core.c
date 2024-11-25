85376595393049d7d8b072a5b4d83e707b57fc19
Vineeth Pillai <vineethrp@google.com>
2024-11-25 09:50:45 -0500

    CHROMIUM: sched: Fix the dl_server reset logic
    dl_server field in task_struct should only be set when the task is
    selected by dlserver. There were couple of code path where the reset was
    not happening and this caused incorrect usage of dlserver. There are
    cases where dl_server was not reset and task migrates to another cpu.
    One such example caused triggering of the following WARNING:
    
     WARNING: CPU: 0 PID: 2682 at kernel/sched/sched.h:1377 update_curr_dl_se+0x4b8/0x6f1
     <TASK>
      update_curr+0x12f/0x327 kernel/sched/fair.c:980
      put_prev_entity+0x3c/0x139 kernel/sched/fair.c:5123
      put_prev_task_fair+0x39/0x5f kernel/sched/fair.c:8132
      put_prev_task kernel/sched/sched.h:2269 [inline]
      put_prev_task_balance+0x12d/0x190 kernel/sched/core.c:6006
      __pick_next_task kernel/sched/core.c:6063 [inline]
      pick_next_task kernel/sched/core.c:6124 [inline]
      __schedule+0x970/0x43a0 kernel/sched/core.c:6703
      preempt_schedule_common+0x74/0xc0 kernel/sched/core.c:6920
      preempt_schedule+0xd6/0xdd kernel/sched/core.c:6944
      preempt_schedule_thunk+0x16/0x18 arch/x86/entry/thunk_64.S:34
      __raw_spin_unlock_irq include/linux/spinlock_api_smp.h:160 [inline]
      _raw_spin_unlock_irq+0x3c/0x3e kernel/locking/spinlock.c:202
      spin_unlock_irq include/linux/spinlock.h:401 [inline]
      call_usermodehelper_exec_async+0x7a/0x336 kernel/umh.c:75
      ret_from_fork+0x1f/0x30 arch/x86/entry/entry_64.S:295
     </TASK>
    
    Fix those paths where dl_server field was not reset correctly.
    
    Upstream has taken care of this by a small rework in dlserver:
    commit bd9bbc96e835 ("sched: Rework dl_server")
    
    Backporting is not trivial as this commit is based on a set of patches
    for sched_ext preparation.
    
    BUG=b:376833598
    TEST=syzkaller repro program doesn't crash
    
    Change-Id: I2a5015fe8d26d8f684e4c8cf7fa11c1eebb0f643
    Signed-off-by: Vineeth Pillai <vineethrp@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6051060
    Reviewed-by: Suleiman Souhlal <suleiman@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3835,8 +3835,6 @@ ttwu_do_activate(struct rq *rq, struct task_struct *p, int wake_flags,
 		rq->idle_stamp = 0;
 	}
 #endif
-
-	p->dl_server = NULL;
 }
 
 /*
@@ -4789,6 +4787,7 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 
 	init_entity_runnable_average(&p->se);
 
+	p->dl_server = NULL;
 
 #ifdef CONFIG_SCHED_INFO
 	if (likely(sched_info_on()))
@@ -6044,17 +6043,11 @@ __pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 
 		/*
 		 * This is a normal CFS pick, but the previous could be a DL pick.
-		 * Clear it as previous is no longer picked.
+		 * Clear it as previous is no longer picked. We also clear this if
+		 * @p == @prev, as this is not a dlserver pick.
 		 */
 		if (prev->dl_server)
 			prev->dl_server = NULL;
-
-		/*
-		 * This is the fast path; it cannot be a DL server pick;
-		 * therefore even if @p == @prev, ->dl_server must be NULL.
-		 */
-		if (p->dl_server)
-			p->dl_server = NULL;
 
 		return p;
 	}
@@ -6132,7 +6125,10 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		 * coming online. core_pick would already be migrated to
 		 * another cpu during offline.
 		 */
-		rq->core_pick = NULL;
+		if (rq->core_pick) {
+			rq->core_pick->dl_server = NULL;
+			rq->core_pick = NULL;
+		}
 		return __pick_next_task(rq, prev, rf);
 	}
 
@@ -6153,6 +6149,7 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		next = rq->core_pick;
 		if (next != prev) {
 			put_prev_task(rq, prev);
+			prev->dl_server = NULL;
 			set_next_task(rq, next);
 		}
 
@@ -6200,7 +6197,10 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 	if (!need_sync) {
 		next = pick_task(rq);
 		if (!next->core_cookie) {
-			rq->core_pick = NULL;
+			if (rq->core_pick) {
+				rq->core_pick->dl_server = NULL;
+				rq->core_pick = NULL;
+			}
 			/*
 			 * For robustness, update the min_vruntime_fi for
 			 * unconstrained picks as well.
