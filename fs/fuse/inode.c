78b69ed6f9e94c37ca892a93973f013a62be8294
Sergey Senozhatsky <senozhatsky@chromium.org>
2024-12-04 13:43:49 +0000

    CHROMIUM: fuse: add hung_request watchdog
    Add a hung_task watchdog to detect stalled request so that
    we can terminate the connection and avoid locking up the
    user-space.
    
    The core part is taken from [1], stripped down to bare
    minimum that we need, and converted to a task-based
    watchdog.
    
    [1] https://lore.kernel.org/all/20241114191332.669127-3-joannelkoong@gmail.com/
    
    UPSTREAM-TASK=b:381968048
    BUG=b:373731719
    TEST=accessed gdrive via Files app on dedede
         kill -STOP $(pidof drivefs)  .. long wait ..
         kill -CONT $(pidof drivefs)
         both before and during fuse req processing
         under lockdep, kasan
    
    Change-Id: I3b0c39af61af89a5e91ce2e21b39899dfb7364a4
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6076182
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Commit-Queue: Tomasz Figa <tfiga@chromium.org>
    (cherry picked from commit 5bc7c6ee93e32cd6f65f271a5a2004a5497a56ac)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6299678

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -694,6 +694,7 @@ void fuse_conn_put(struct fuse_conn *fc)
 	if (refcount_dec_and_test(&fc->count)) {
 		struct fuse_iqueue *fiq = &fc->iq;
 
+		terminate_fuse_watchdog(fc);
 		if (IS_ENABLED(CONFIG_FUSE_DAX))
 			fuse_dax_conn_free(fc);
 		if (fiq->ops->release)
@@ -1266,6 +1267,7 @@ int fuse_fill_super_common(struct super_block *sb, struct fuse_fs_context *ctx)
 	fc->no_control = ctx->no_control;
 	fc->no_force_umount = ctx->no_force_umount;
 	fc->init_security = ctx->init_security;
+	init_fuse_watchdog(fc);
 
 	err = -ENOMEM;
 	root = fuse_get_root_inode(sb, ctx->rootmode);
