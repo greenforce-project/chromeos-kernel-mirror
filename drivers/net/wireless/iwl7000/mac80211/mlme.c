261ac152272871b489e994043619cb82ff5fc5e9
Johannes Berg <johannes.berg@intel.com>
2025-01-13 11:23:50 +0100

    CHROMIUM: iwl7000: mac80211: mlme: support extended MLD capa/ops in assoc
    Support passing the value from userspace on to the AP in the
    association and ML link reconfiguration requests. We may need
    to also add a driver value to or in with the field, but for
    now have no feature that is controlled by the driver.
    
    Change-Id: I623f59023b47ec202fc0c7520f2b5f575b439927
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: b8c7f736147eb015b664f79b1821d819919b6f5c

--- drivers/net/wireless/iwl7000/mac80211/mlme.c
+++ drivers/net/wireless/iwl7000/mac80211/mlme.c
@@ -1946,6 +1946,14 @@ ieee80211_assoc_add_ml_elem(struct ieee80211_sub_if_data *sdata,
 	}
 	skb_put_data(skb, &mld_capa_ops, sizeof(mld_capa_ops));
 
+	if (assoc_data->ext_mld_capa_ops) {
+		ml_elem->control |=
+			cpu_to_le16(IEEE80211_MLC_BASIC_PRES_EXT_MLD_CAPA_OP);
+		common->len += 2;
+		skb_put_data(skb, &assoc_data->ext_mld_capa_ops,
+			     sizeof(assoc_data->ext_mld_capa_ops));
+	}
+
 	for (link_id = 0; link_id < IEEE80211_MLD_MAX_NUM_LINKS; link_id++) {
 		u16 link_present_elems[PRESENT_ELEMS_MAX] = {};
 		const u8 *extra_elems;
@@ -2115,6 +2123,7 @@ static int ieee80211_send_assoc(struct ieee80211_sub_if_data *sdata)
 		/* max common info field in basic multi-link element */
 		size += sizeof(struct ieee80211_mle_basic_common_info) +
 			2 + /* capa & op */
+			2 + /* ext capa & op */
 			2; /* EML capa */
 
 		/*
@@ -9398,6 +9407,8 @@ int ieee80211_mgd_assoc(struct ieee80211_sub_if_data *sdata,
 	else
 		memcpy(assoc_data->ap_addr, cbss->bssid, ETH_ALEN);
 
+	assoc_data->ext_mld_capa_ops = 0;
+
 	if (ifmgd->associated) {
 		u8 frame_buf[IEEE80211_DEAUTH_FRAME_LEN];
 
@@ -10215,6 +10226,10 @@ ieee80211_build_ml_reconf_req(struct ieee80211_sub_if_data *sdata,
 		if (eml_capa & cpu_to_le16((IEEE80211_EML_CAP_EMLSR_SUPP |
 					    IEEE80211_EML_CAP_EMLMR_SUPPORT)))
 			var_common_size += 2;
+
+		if (add_links_data->ext_mld_capa_ops)
+			var_common_size +=
+				sizeof(add_links_data->ext_mld_capa_ops);
 	}
 
 	/* Add the common information length */
@@ -10299,6 +10314,13 @@ ieee80211_build_ml_reconf_req(struct ieee80211_sub_if_data *sdata,
 			cpu_to_le16(IEEE80211_MLC_RECONF_PRES_MLD_CAPA_OP);
 
 		skb_put_data(skb, &mld_capa_ops, sizeof(mld_capa_ops));
+
+		if (add_links_data->ext_mld_capa_ops) {
+			ml_elem->control |=
+				cpu_to_le16(IEEE80211_MLC_BASIC_PRES_EXT_MLD_CAPA_OP);
+			skb_put_data(skb, &add_links_data->ext_mld_capa_ops,
+				     sizeof(add_links_data->ext_mld_capa_ops));
+		}
 	}
 
 	if (sdata->u.mgd.flags & IEEE80211_STA_ENABLE_RRM)
@@ -10437,6 +10459,8 @@ int ieee80211_mgd_assoc_ml_reconf(struct ieee80211_sub_if_data *sdata,
 		data = kzalloc(sizeof(*data), GFP_KERNEL);
 		if (!data)
 			return -ENOMEM;
+
+		data->ext_mld_capa_ops = 0;
 
 		uapsd_supported = true;
 		ieee80211_ml_reconf_selectors(&userspace_selectors);
