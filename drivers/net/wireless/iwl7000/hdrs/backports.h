fce6d43ce466f38be9440dcd386687880f1f7865
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-02-01 21:35:36 +0200

    CHROMIUM: iwl7000: fix DEFINE_LOCK_GUARD_0 backport
    This was added in 6.1, not in 5.5.
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: Ifb3e945742c9d94b4644963992e8bbf825d86ec7
    iwl7000-tree: 6b6bb6e1eb523564e099a3f46cb04b3af8f20100

--- drivers/net/wireless/iwl7000/hdrs/backports.h
+++ drivers/net/wireless/iwl7000/hdrs/backports.h
@@ -472,6 +472,20 @@ static inline void backport_netif_napi_add(struct net_device *dev,
 #include <linux/cleanup.h>
 DEFINE_FREE(kfree, void *, if (!IS_ERR_OR_NULL(_T)) kfree(_T))
 
+DEFINE_LOCK_GUARD_0(rcu,
+	do {
+		rcu_read_lock();
+		/*
+		 * sparse doesn't call the cleanup function,
+		 * so just release immediately and don't track
+		 * the context. We don't need to anyway, since
+		 * the whole point of the guard is to not need
+		 * the explicit unlock.
+		 */
+		__release(RCU);
+	} while (0),
+	rcu_read_unlock())
+
 #define SKB_CONSUMED (SKB_DROP_REASON_MAX + 1)
 #define VISIBLE_IF_KUNIT static
 #define EXPORT_SYMBOL_IF_KUNIT(...)
