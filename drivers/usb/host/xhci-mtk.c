d62176151c37a7a4743304ef41a5e97522c97c1d
Fei Shao <fshao@chromium.org>
2025-03-19 03:15:15 -0700

    Revert "FROMLIST: usb: mtk-xhci: add support remote wakeup of mt8196"
    This reverts commit c81ef45139a0f33cb7be7f52feb35a7b07f1f062.
    
    Reason for revert: Apply new FROMLIST
    
    Original change's description:
    > FROMLIST: usb: mtk-xhci: add support remote wakeup of mt8196
    >
    > There are 2 USB controllers on mt8196, each controller's wakeup control is
    > different, add some specific versions for them.
    >
    > Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    > (am from https://patchwork.kernel.org/patch/13885450/)
    > (also found at https://lore.kernel.org/r/20241126025507.29605-2-chunfeng.yun@mediatek.com)
    >
    > BUG=b:357046538
    > TEST=emerge-rauru sys-kernel/chromeo-kernel-6_6
    > UPSTREAM-TASK=b:387403117
    >
    > Change-Id: I9bd43f501e111f6a676316d17bec2488cc9449d4
    > Signed-off-by: Liu Liu <ot_liu.liu@mediatek.corp-partner.google.com>
    > Signed-off-by: Fei Shao <fshao@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6073710
    > Reviewed-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    > Reviewed-by: Chen-Yu Tsai <wenst@chromium.org>
    > Reviewed-by: Sean Paul <sean@poorly.run>
    > Reviewed-by: Yidi Lin <yidilin@google.com>
    
    BUG=b:357046538
    BUG=b:391779188
    
    Change-Id: If0dca11efdeca67f173bd2e99acf2f5197ea0925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6370766
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Reviewed-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Commit-Queue: Chen-Yu Tsai <wenst@chromium.org>
    Reviewed-by: Chen-Yu Tsai <wenst@chromium.org>
    Tested-by: Fei Shao <fshao@chromium.org>

--- drivers/usb/host/xhci-mtk.c
+++ drivers/usb/host/xhci-mtk.c
@@ -113,12 +113,6 @@
 #define WC1_IS_P_95		BIT(12)
 #define WC1_IS_EN_P0_95		BIT(6)
 
-/* mt8196 */
-#define PERI_WK_CTRL0_8196	0x08
-#define UWK_V1_7_CTRL2_MASK	0x5
-
-#define WCP1_IS_EN		BIT(7) /* port1 en bit */
-
 /* mt2712 etc */
 #define PERI_SSUSB_SPM_CTRL	0x0
 #define SSC_IP_SLEEP_EN	BIT(4)
@@ -135,8 +129,6 @@ enum ssusb_uwk_vers {
 	SSUSB_UWK_V1_4,		/* mt8195 IP1 */
 	SSUSB_UWK_V1_5,		/* mt8195 IP2 */
 	SSUSB_UWK_V1_6,		/* mt8195 IP3 */
-	SSUSB_UWK_V1_7,		/* mt8196 IP0 */
-	SSUSB_UWK_V1_8,		/* mt8196 IP1 */
 };
 
 /*
@@ -388,16 +380,6 @@ static void usb_wakeup_ip_sleep_set(struct xhci_hcd_mtk *mtk, bool enable)
 		reg = mtk->uwk_reg_base + PERI_WK_CTRL0_8195;
 		msk = WC0_IS_EN_P3_95 | WC0_IS_C_95(0x7) | WC0_IS_P_95;
 		val = enable ? (WC0_IS_EN_P3_95 | WC0_IS_C_95(0x1)) : 0;
-		break;
-	case SSUSB_UWK_V1_7:
-		reg = mtk->uwk_reg_base + PERI_WK_CTRL0_8196;
-		msk = UWK_V1_7_CTRL2_MASK;
-		val = enable ? msk : 0;
-		break;
-	case SSUSB_UWK_V1_8:
-		reg = mtk->uwk_reg_base + PERI_WK_CTRL0_8196;
-		msk = WCP1_IS_EN;
-		val = enable ? msk : 0;
 		break;
 	case SSUSB_UWK_V2:
 		reg = mtk->uwk_reg_base + PERI_SSUSB_SPM_CTRL;
