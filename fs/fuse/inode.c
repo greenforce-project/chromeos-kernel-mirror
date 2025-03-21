be1c53feaca45831ffc003f2e6a518e13956e5c8
Sergey Senozhatsky <senozhatsky@chromium.org>
2025-03-21 05:07:29 +0000

    CHROMIUM: fuse: init fusedog on successful FUSE_INIT
    Mounting a very large archive, for example ChromeOS tar image,
    is time unbound (scan + decompression).  Monitoring connection
    at this (when we wait for superblock to be setup) stage triggers
    opcode FUSE_INIT request timeout and makes it impossible to open
    very large archives.  Postpone fusedog initialization and start
    monitoring connection only after opcode FUSE_INIT finishes.
    
    UPSTREAM-TASK=b:381968048
    BUG=b:373731719, b:401260020
    TEST=fuse-archive decompressed large ChromeOS tar file
         also made sure that we still reset hung requests
    
    Conflicts:
        fs/fuse/inode.c
    
    Change-Id: Id585ebead9b62debc7fd62a761673958b080de16
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6380333
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    (cherry picked from commit c5805886f81339e356aa39ed986fd7528ed20807)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6381413
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1339,6 +1339,8 @@ static void process_init_reply(struct fuse_mount *fm, struct fuse_args *args,
 	if (!ok) {
 		fc->conn_init = 0;
 		fc->conn_error = 1;
+	} else {
+		init_fuse_watchdog(fc);
 	}
 
 	fuse_set_initialized(fc);
@@ -1724,7 +1726,6 @@ int fuse_fill_super_common(struct super_block *sb, struct fuse_fs_context *ctx)
 	fc->destroy = ctx->destroy;
 	fc->no_control = ctx->no_control;
 	fc->no_force_umount = ctx->no_force_umount;
-	init_fuse_watchdog(fc);
 
 	err = -ENOMEM;
 	root = fuse_get_root_inode(sb, ctx->rootmode);
