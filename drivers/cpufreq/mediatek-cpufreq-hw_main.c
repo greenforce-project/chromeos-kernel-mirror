8c9a15df1fc6a6509911807257f058a7b522a238
Kai Liang <kai.liang@mediatek.corp-partner.google.com>
2025-04-29 21:18:45 +0800

    CHROMIUM: cpufreq: mediatek: Build MediaTek cpufreq driver as a module
    Allow the MediaTek cpufreq driver to be built as a module.
    
    BUG=b:404347897
    UPSTREAM-TASK=b:383958462
    TEST=build pass and cpufreq driver functionality verified
    
    Signed-off-by: Kai Liang <kai.liang@mediatek.corp-partner.google.com>
    Change-Id: I3a33096a67c991bf3b4601ae12ff8f7d56868e1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6497896
    Reviewed-by: Fei Shao <fshao@chromium.org>
    Reviewed-by: Yu-Che Cheng <giver@chromium.org>

--- drivers/cpufreq/mediatek-cpufreq-hw_main.c
+++ drivers/cpufreq/mediatek-cpufreq-hw_main.c
@@ -701,6 +701,7 @@ static const struct of_device_id mtk_cpufreq_hw_match[] = {
 	{ .compatible = "mediatek,cpufreq-hw-v1", .data = &cpufreq_mtk_offsets },
 	{}
 };
+MODULE_DEVICE_TABLE(of, mtk_cpufreq_hw_match);
 
 static struct platform_driver mtk_cpufreq_hw_driver = {
 	.probe = mtk_cpufreq_hw_driver_probe,
