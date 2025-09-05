39cbc63b37a9ae3b1f0c0c1d14b51dfd3855b060
David Wu <david_wu@quanta.corp-partner.google.com>
2025-09-05 19:36:35 +0800

    CHROMIUM: platform/chrome: of_hw_prober: Support TP and TS probing on Baze
    The MT8189 Baze Chromebook have second source trackpads and
    touchscreens that need to be probed. Let the prober probe for them.
    
    BUG=b:433136990
    TEST=emerge-skywalker sys-kernel/chromeos-kernel-6_6
    UPSTREAM-TASK=b:440284724
    
    Change-Id: I39c710859d8fc0ea9591f98eb6f38ee4b3291a46
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6918296
    Reviewed-by: Cathy Chen <cathy_chen@quanta.corp-partner.google.com>
    Commit-Queue: Shawn Ku <shawnku@chromium.org>
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-by: Tony Chang <tonytcc@google.com>
    Reviewed-by: Shawn Ku <shawnku@chromium.org>
    Reviewed-by: Chen-Yu Tsai <wenst@chromium.org>

--- drivers/platform/chrome/chromeos_of_hw_prober.c
+++ drivers/platform/chrome/chromeos_of_hw_prober.c
@@ -110,6 +110,17 @@ static const struct chromeos_i2c_probe_data chromeos_i2c_probe_yoda_touchscreen 
 	},
 };
 
+static const struct chromeos_i2c_probe_data chromeos_i2c_probe_baze_touchscreen = {
+	.cfg = &chromeos_i2c_probe_simple_touchscreen_cfg,
+	.opts = &(const struct i2c_of_probe_simple_opts) {
+		.res_node_compatible = "ilitek,ili2901",
+		.supply_name = "vcc33",
+		.gpio_name = "reset",
+		.post_power_on_delay_ms = 10,
+		.post_gpio_config_delay_ms = 100,
+	},
+};
+
 static const struct hw_prober_entry hw_prober_platforms[] = {
 	{
 		.compatible = "google,hana",
@@ -160,6 +171,14 @@ static const struct hw_prober_entry hw_prober_platforms[] = {
 		.compatible = "google,yoda",
 		.prober = chromeos_i2c_component_prober,
 		.data = &chromeos_i2c_probe_yoda_touchscreen,
+	}, {
+		.compatible = "google,baze",
+		.prober = chromeos_i2c_component_prober,
+		.data = &chromeos_i2c_probe_hana_trackpad,
+	}, {
+		.compatible = "google,baze",
+		.prober = chromeos_i2c_component_prober,
+		.data = &chromeos_i2c_probe_baze_touchscreen,
 	}
 };
 
